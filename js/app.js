01617714bcd43a77125ba330a155f1a26713f866
James Swift <sbcjames86@yahoo.com>
2020-08-06 15:10:31 -0700

    update to alert on q1-q3 and wrapped function on q3

--- js/app.js
+++ js/app.js
@@ -11,9 +11,11 @@ whatIsYourName()
 //question1//
 function goodMorning(){
   var question1= prompt('Are you having a good morning?');
-  if(question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y'){ ('that is great, you will have a great day!');
+  if(question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y'){
+    alert('that is great, you will have a great day!');
   }
-  else if (question1.toLowerCase() === 'no' ||question1.toLowerCase() === 'n'){ ('That is ok because you are about to have a great day');
+  else if (question1.toLowerCase() === 'no' ||question1.toLowerCase() === 'n'){
+    alert('That is ok because you are about to have a great day');
   }
   else alert('I dont know what that was, but hang in there and try y or n or yes or no');
 }
@@ -23,9 +25,11 @@ goodMorning()
 //question 2//
 function practiceCoding(){
   var question2= prompt('Are you practicing your coding?');
-  if(question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y'){ ('that is great, you are gonna be a pro someday!');
+  if(question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y'){
+    alert('that is great, you are gonna be a pro someday!');
   }
-  else if (question2.toLowerCase() === 'no' ||question2.toLowerCase() === 'n'){  ('That is ok keep practicing and do not forget to ask for help');
+  else if (question2.toLowerCase() === 'no' ||question2.toLowerCase() === 'n'){
+    alert('That is ok keep practicing and do not forget to ask for help');
   }
   else alert('I dont know what that was, but hang in there and try y or n or yes or no');
 }
@@ -33,10 +37,18 @@ function practiceCoding(){
 practiceCoding()
 
 //question 3//
-var question3= prompt('Have you started creating a support network?');
-if(question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y'){answer3 = 'that is great, you are gonna make a ton of friends!';}
-else if (question3.toLowerCase() === 'no' ||question3.toLowerCase() === 'n'){answer1 = 'That is ok, do not be afraid to reach out to people in your industry';}
-else alert('I dont know what that was, but hang in there and try y or n or yes or no');
+function supportNetwork(){
+  var question3= prompt('Have you started creating a support network?');
+  if(question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y'){
+    alert('that is great, you are gonna make a ton of friends!');
+  }
+  else if (question3.toLowerCase() === 'no' ||question3.toLowerCase() === 'n'){
+    alert('That is ok, do not be afraid to reach out to people in your industry');
+  }
+  else alert('I dont know what that was, but hang in there and try y or n or yes or no');
+}
+
+supportNetwork()
 
 //question 4//
 var question4= prompt('Are you keeping a positive mental attitude?');
